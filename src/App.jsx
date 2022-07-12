1da4b38ddb3da7d1e60176149488ba71a78e8846
nikolai-chernolutskii <nikolai@site-now.online>
2022-07-12 22:44:40 +0200

    Implement search filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,18 @@ class App extends Component {
     this.state = {
       monsters: [], // Initial state of array will be empty prior to receiving external data, the null state
     };
+
+    console.log('constructor')
   }
 
   componentDidMount() {
+
+    console.log('componentDidMount')
+
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
+      .then((users) => this.setState(
+        () => {
         return { monsters: users }
       },
         () => {
@@ -25,17 +31,46 @@ class App extends Component {
   }
 
   render() {
+
+    console.log('render')
+
+    
+
     return (
       <div className="App">
+
+        <input className='search-box' type='search' placeholder='Search monsters'
+
+          onChange={
+            (event) /* We are going to get back an event */ => {
+            console.log(event);
+
+            const searchString = event.target.value.toLocaleLowerCase(); /* describing the search string and making it lowercase */
+
+            const filteredMonsters = this.state.monsters.filter(
+              (monster) => {
+                return monster.name.toLocaleLowerCase().includes(searchString);
+              }); /* describing the list of filtered monsters including the search string */
+
+            this.setState(
+              () => {
+              return { monsters: filteredMonsters };
+            }) /* setting the state to the list of filtered monsters */
+          }
+          }
+
+        />
+
         {
           this.state.monsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
               </div>
-            )
-          })
-        }
+            ) 
+          }) /* describing the unfiltered list using the .map method */
+        } 
+        
       </div>
     )
   }
