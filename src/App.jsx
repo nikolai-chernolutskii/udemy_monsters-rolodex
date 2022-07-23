103e6f9c319c7d11029b4b6301f5c97316251726
nikolai-chernolutskii <nikolai@site-now.online>
2022-07-23 11:38:03 +0200

    Store original data and code optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -33,44 +33,47 @@ class App extends Component {
       ));
   }
 
+  onSearchChange = (event) => {
+              
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(
+      () => {
+        return { searchField };
+      });
+  } // Moving this function into the class so that it initialized just once, on the first initialization of the class
+
+
   render() {
 
     console.log('render')
 
-    const filteredMonsters = this.state.monsters.filter(
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(
       (monster) => {
-        return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-      }); /* describing the list of filtered monsters including the _empty_ search field */
-    // We move this variable outside the return (below) because we always want to be able to come back to the original state when we unfilter the new array
+        return monster.name.toLocaleLowerCase().includes(searchField);
+      });
 
     return (
       <div className="App">
 
         <input className='search-box' type='search' placeholder='Search monsters'
 
-          onChange={
-            (event) /* We are going to get back an event */ => {
-              console.log(event);
-
-              const searchField = event.target.value.toLocaleLowerCase(); /* describing the _non-empty_ search field and making it lowercase */
-
-              this.setState(
-                () => {
-                  return { searchField };
-                }) /* setting the state to the list of _filtered_ monsters */
-            }
-          }
+          onChange={onSearchChange}
 
         />
 
         {
-          filteredMonsters.map((monster) => {
+          filteredMonsters.map(
+            (monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
               </div>
             )
-          }) /* Then we map over the _filteredMonsters_ constant instead of _this.state_, describing the unfiltered list using the .map method */
+          })
         }
 
       </div>
